151963ec9ab39533190f9d53ea9226d0144d6abf
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-07-26 15:39:37 +0200

    Fix(kernel): include gdt.h for gdt_init prototype

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include "vga.h"
+#include "gdt.h"
 
 void kernel_main(void) {
     gdt_init();
@@ -7,6 +8,6 @@ void kernel_main(void) {
     // 30 righe su 25 di schermo, così si vede se lo scroll funziona
     for (int i = 0; i < 30; i++) {
         vga_putchar('A' + i);
-        vga_write(" riga\n");
+        vga_write(" row\n");
     }
 }
